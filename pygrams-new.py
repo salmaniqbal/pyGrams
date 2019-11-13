da90589960a189191393c53801b621081f4f29d6
finnkauski <iwiifi@gmail.com>
2019-11-13 15:24:18 +0000

    make pipelien a function

--- pygrams-new.py
+++ pygrams-new.py
@@ -4,8 +4,9 @@ import os
 import time
 import pickle
 
-from scripts.pipeline import Pipeline
+from scripts.pipeline import Pipeline, proc, output
 from scripts.utils.pygrams_exception import PygramsException
+
 
 def rubbish_create_folds():
     paths = [
@@ -18,6 +19,7 @@ def rubbish_create_folds():
     for path in paths:
         os.makedirs(path, exist_ok=True)
 
+
 def main():
     rubbish_create_folds()
 
@@ -25,35 +27,52 @@ def main():
     with open("args.pkl", "rb") as handle:
         args = pickle.load(handle)
 
-    outputs = ['reports', 'json_config']
+    outputs = ["reports", "json_config"]
 
-    docs_mask_dict = {'filter_by': 'intersection', 'cpc': None, 'cite': None, 'columns': None, 'date': None, 'timeseries_date': None, 'date_header': None}
+    docs_mask_dict = {
+        "filter_by": "intersection",
+        "cpc": None,
+        "cite": None,
+        "columns": None,
+        "date": None,
+        "timeseries_date": None,
+        "date_header": None,
+    }
     terms_mask_dict = None
 
     doc_source_file_name = os.path.join(args.path, args.doc_source)
 
-    pipeline = Pipeline(
-        'data/USPTO-random-1000.pkl.bz2',
-        {'filter_by': 'intersection', 'cpc': None, 'cite': None, 'columns': None, 'date': None, 'timeseries_date': None, 'date_header': None},
-        pick_method='sum',
+    results = proc(
+        "data/USPTO-random-1000.pkl.bz2",
+        {
+            "filter_by": "intersection",
+            "cpc": None,
+            "cite": None,
+            "columns": None,
+            "date": None,
+            "timeseries_date": None,
+            "date_header": None,
+        },
+        pick_method="sum",
         ngram_range=(1, 3),
-        text_header='abstract',
+        text_header="abstract",
         cached_folder_name=None,
         max_df=0.05,
         user_ngrams=[],
         prefilter_terms=100000,
         terms_threshold=0.75,
-        output_name='out',
+        output_name="out",
         calculate_timeseries=False,
         m_steps_ahead=5,
-        emergence_index='porter',
+        emergence_index="porter",
         exponential=False,
         nterms=25,
         patents_per_quarter_threshold=15,
-        sma='savgol'
+        sma="savgol",
     )
 
-    pipeline.output(
+    output(
+        *results,
         outputs,
         wordcloud_title="Popular Terms",
         outname="out",
@@ -61,9 +80,6 @@ def main():
         n_nmf_topics=0,
     )
 
-    outputs_name = pipeline.outputs_folder_name
-
-    print(outputs_name)
 
 try:
     start = time.time()
